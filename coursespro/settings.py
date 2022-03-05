947b67dbb74b2e90c76fbb0f4568645f3f6200a4
Fattijenishbek <fattijenishbek01@gmail.com>
2022-03-05 17:21:23 +0600

    added procfile and requirements

--- coursespro/settings.py
+++ coursespro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=e69wf7%bh@h*t()x-pw!-)&w&pct0*m4=lr(@xv36p1tq0wi@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
